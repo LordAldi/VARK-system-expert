362dbb947333999a572acfde3efc823e57984434
LordAldi <aldianugra09@gmail.com>
2020-10-16 22:09:08 +0700

    fix quiz only one

--- js/quiz.js
+++ js/quiz.js
@@ -7,213 +7,123 @@ let quiz = [
     c: "write down the directions (without a map).",
     d: "go with her.",
   },
-  // {
-  //   soal:
-  //     "You are not sure whether a word should be spelled `dependent' or `dependant'. You would:",
-  //   a: "see the word in your mind and choose by the way they look.",
-  //   b: "think about how each word sounds and choose one.",
-  //   c: "find it online or in a dictionary.",
-  //   d: "write both words on paper and choose one.",
-  // },
-  // {
-  //   soal:
-  //     "You are planning a holiday for a group. You want some feedback from them about the plan.  You would:",
-  //   a: "use a map or website to show them the places.",
-  //   b: "phone, text or email them.",
-  //   c: "give them a copy of the printed itinerary.",
-  //   d: "describe some of the highlights.",
-  // },
-  // {
-  //   soal:
-  //     "You are going to cook something as a special treat for your family. You would:",
-  //   a: "look through the cookbook for ideas from the picture",
-  //   b: "ask friends for suggestions.",
-  //   c: "use a cookbook where you know there is a good recipe.",
-  //   d: "cook something you know without the need for instructions.",
-  // },
-  // {
-  //   soal:
-  //     " A group of tourists want to learn about the parks or wildlife reserves in your area. You would:",
-  //   a: "show them internet pictures, photographs or picture books.",
-  //   b:
-  //     "talk about, or arrange a talk for them about parks or wildlife reserves.",
-  //   c: "give them a book or pamphlets about the parks or wildlife reserves.",
-  //   d: "take them to a park or wildlife reserve and walk with them.",
-  // },
-  // {
-  //   soal:
-  //     "You are about to purchase a digital camera or mobile phone. Other than price, what would most influence your decision?",
-  //   a: "It is a modern design and looks good.",
-  //   b: "The salesperson telling me about its features.",
-  //   c: "Reading the details about its features.",
-  //   d: "Trying or testing it.",
-  // },
-  // {
-  //   soal:
-  //     "Remember a time when you learned how to do something new. Try to avoid choosing a physical skill, e.g.riding a bike. You learned best by:",
-  //   a: "diagrams and charts -visual clues.",
-  //   b: "listening to somebody explaining it and asking questions.",
-  //   c: "written instructions –e.g. a manual or textbook.",
-  //   d: "watching a demonstration.",
-  // },
-  // {
-  //   soal:
-  //     "You have a problem with your knee. You would prefer that the doctor:",
-  //   a: "showed you a diagram of what was wrong.",
-  //   b: "described what was wrong.",
-  //   c: "gave you a web address or something to read about it.",
-  //   d: "used a plastic model of a knee to show what was wrong.",
-  // },
-  // {
-  //   soal:
-  //     "You want to learn a new program, skill or game on a computer. You would:",
-  //   a: "follow the diagrams in the book that came with it.",
-  //   b: "talk with people who know about the program.",
-  //   c: "read the written instructions that came with the program.",
-  //   d: "use the controls or keyboard.",
-  // },
-  // {
-  //   soal: "I like websites that have:",
-  //   a: "interesting design and visual features.",
-  //   b: "audio channels where I can hear music, radio programs or interviews.",
-  //   c: "interesting written descriptions, lists and explanations.",
-  //   d: "things I can click on, shift or try.",
-  // },
-  // {
-  //   soal:
-  //     "Other than price, what would most influence your decision to buy a new non-fiction book?",
-  //   a: "The way it looks is appealing.",
-  //   b: "A friend talks about it and recommends it.",
-  //   c: "Quickly reading parts of it.",
-  //   d: "It has real-life stories, experiences and examples.",
-  // },
-  // {
-  //   soal:
-  //     "You are using a book, CD or website to learn how to take photos with your new digital camera.  You would like to have:",
-  //   a: "diagrams showing the camera and what each part does.",
-  //   b: "a chance to ask questions and talk about the camera and its features.",
-  //   c:
-  //     "clear written instructions with lists and bullet points about what to do.",
-  //   d: "many examples of good and poor photos and how to improve them.",
-  // },
-  // {
-  //   soal: "Do you prefer a teacher or a presenter who uses:",
-  //   a: "diagrams, charts or graphs.",
-  //   b: "question and answer, talk, group discussion, or guest speakers.",
-  //   c: "handouts, books, or readings.",
-  //   d: "demonstrations, models or practical sessions.",
-  // },
-  // {
-  //   soal:
-  //     "You have finished a competition or test and would like some feedback. You would like to have feedback:",
-  //   a: "using graphs showing what you had achieved.",
-  //   b: "from somebody who talks it through with you.",
-  //   c: "using a written description of your results.",
-  //   d: "using examples from what you have done.",
-  // },
-  // {
-  //   soal: "You are going to choose food at a restaurant or cafe. You would:",
-  //   a: "look at what others are eating or look at pictures of each dish.",
-  //   b: "ask the waiter or friends to recommend choices.",
-  //   c: "choose from the descriptions in the menu.",
-  //   d: "choose something that you have had there before.",
-  // },
-  // {
-  //   soal:
-  //     "You have to make an important speech at a conference or special occasion. You would:",
-  //   a: "make diagrams or get graphs to help explain things.",
-  //   b: "write a few key words and practice saying your speech over and over.",
-  //   c: "write out your speech and learn from reading it over several times.",
-  //   d: "gather many examples and stories to make the talk real and practical.",
-  // },
+  {
+    soal:
+      "You are not sure whether a word should be spelled `dependent' or `dependant'. You would:",
+    a: "see the word in your mind and choose by the way they look.",
+    b: "think about how each word sounds and choose one.",
+    c: "find it online or in a dictionary.",
+    d: "write both words on paper and choose one.",
+  },
+  {
+    soal:
+      "You are planning a holiday for a group. You want some feedback from them about the plan.  You would:",
+    a: "use a map or website to show them the places.",
+    b: "phone, text or email them.",
+    c: "give them a copy of the printed itinerary.",
+    d: "describe some of the highlights.",
+  },
+  {
+    soal:
+      "You are going to cook something as a special treat for your family. You would:",
+    a: "look through the cookbook for ideas from the picture",
+    b: "ask friends for suggestions.",
+    c: "use a cookbook where you know there is a good recipe.",
+    d: "cook something you know without the need for instructions.",
+  },
+  {
+    soal:
+      " A group of tourists want to learn about the parks or wildlife reserves in your area. You would:",
+    a: "show them internet pictures, photographs or picture books.",
+    b:
+      "talk about, or arrange a talk for them about parks or wildlife reserves.",
+    c: "give them a book or pamphlets about the parks or wildlife reserves.",
+    d: "take them to a park or wildlife reserve and walk with them.",
+  },
+  {
+    soal:
+      "You are about to purchase a digital camera or mobile phone. Other than price, what would most influence your decision?",
+    a: "It is a modern design and looks good.",
+    b: "The salesperson telling me about its features.",
+    c: "Reading the details about its features.",
+    d: "Trying or testing it.",
+  },
+  {
+    soal:
+      "Remember a time when you learned how to do something new. Try to avoid choosing a physical skill, e.g.riding a bike. You learned best by:",
+    a: "diagrams and charts -visual clues.",
+    b: "listening to somebody explaining it and asking questions.",
+    c: "written instructions –e.g. a manual or textbook.",
+    d: "watching a demonstration.",
+  },
+  {
+    soal:
+      "You have a problem with your knee. You would prefer that the doctor:",
+    a: "showed you a diagram of what was wrong.",
+    b: "described what was wrong.",
+    c: "gave you a web address or something to read about it.",
+    d: "used a plastic model of a knee to show what was wrong.",
+  },
+  {
+    soal:
+      "You want to learn a new program, skill or game on a computer. You would:",
+    a: "follow the diagrams in the book that came with it.",
+    b: "talk with people who know about the program.",
+    c: "read the written instructions that came with the program.",
+    d: "use the controls or keyboard.",
+  },
+  {
+    soal: "I like websites that have:",
+    a: "interesting design and visual features.",
+    b: "audio channels where I can hear music, radio programs or interviews.",
+    c: "interesting written descriptions, lists and explanations.",
+    d: "things I can click on, shift or try.",
+  },
+  {
+    soal:
+      "Other than price, what would most influence your decision to buy a new non-fiction book?",
+    a: "The way it looks is appealing.",
+    b: "A friend talks about it and recommends it.",
+    c: "Quickly reading parts of it.",
+    d: "It has real-life stories, experiences and examples.",
+  },
+  {
+    soal:
+      "You are using a book, CD or website to learn how to take photos with your new digital camera.  You would like to have:",
+    a: "diagrams showing the camera and what each part does.",
+    b: "a chance to ask questions and talk about the camera and its features.",
+    c:
+      "clear written instructions with lists and bullet points about what to do.",
+    d: "many examples of good and poor photos and how to improve them.",
+  },
+  {
+    soal: "Do you prefer a teacher or a presenter who uses:",
+    a: "diagrams, charts or graphs.",
+    b: "question and answer, talk, group discussion, or guest speakers.",
+    c: "handouts, books, or readings.",
+    d: "demonstrations, models or practical sessions.",
+  },
+  {
+    soal:
+      "You have finished a competition or test and would like some feedback. You would like to have feedback:",
+    a: "using graphs showing what you had achieved.",
+    b: "from somebody who talks it through with you.",
+    c: "using a written description of your results.",
+    d: "using examples from what you have done.",
+  },
+  {
+    soal: "You are going to choose food at a restaurant or cafe. You would:",
+    a: "look at what others are eating or look at pictures of each dish.",
+    b: "ask the waiter or friends to recommend choices.",
+    c: "choose from the descriptions in the menu.",
+    d: "choose something that you have had there before.",
+  },
+  {
+    soal:
+      "You have to make an important speech at a conference or special occasion. You would:",
+    a: "make diagrams or get graphs to help explain things.",
+    b: "write a few key words and practice saying your speech over and over.",
+    c: "write out your speech and learn from reading it over several times.",
+    d: "gather many examples and stories to make the talk real and practical.",
+  },
 ];
-
-// 4.You are going to cook something as a special treat for your family. (Kamu
-// akan memasak sesuatu sebagai suguhan spesial untuk keluargamu). You
-// would:
-// a) look through the cookbook for ideas from the pictures.
-// b) ask friends for suggestions.
-// c) use a cookbook where you know there is a good recipe.
-// d) cook something you know without the need for instructions.
-// 5.A group of tourists want to learn about the parks or wildlife reserves in
-// your area. (Sebuah kelompok wisatawan ingin mempelajari tentang kebun
-// raya atau cagar alam di daerahmu). You would:
-// a)show them internet pictures, photographs or picture books.
-// b)talk about, or arrange a talk for them about parks or wildlife reserves.
-// c) give them a book or pamphlets about the parks or wildlife reserves.
-// d)take them to a park or wildlife reserve and walk with them.
-// 6.You are about to purchase a digital camera or mobile phone. Other than
-// price, what would most influence yourdecision?(Kamu membeli kamera
-// digital atau telepon genggam. Selain harga, apa yang paling mempengaruhi
-// keputusanmu?)
-// a) It is a modern design and looks good.
-// b)The salesperson telling me about its features.
-// c) Reading the details about its features.
-// d)Trying or testing it.
-// 7.Remember a time when you learned how to do something new. Try to avoid
-// choosing a physical skill, e.g.riding a bike. (Ingat saat kamu belajar cara
-// melakukan sesuatu yang baru. Coba untuk menghindari pemilihan
-// keterampilan fisik, seperti bersepeda). You learned best by:
-// a) diagrams and charts -visual clues.
-// b)listening to somebody explaining it and asking questions.
-// c) written instructions –e.g. a manual or textbook.
-// d)watching a demonstration.
-// 8.You have a problem with your knee. (Kamu memiliki masalah dengan
-// lututmu). You would prefer that the doctor:
-// a)showed you a diagram of what was wrong.
-// b) described what was wrong.
-// c) gave you a web address or something to read about it.
-// d) used a plastic model of a knee to show what was wrong.
-// 9.You want to learn a new program, skill or game on a computer.(Kamu
-// ingin belajar program baru, keterampilan atau permainan di komputer).
-// You would.
-// a) follow the diagrams in the book that came with it.
-// b)talk with people who know about the program.
-// c) read the written instructions that came with the program.
-// d) use the controls or keyboard.
-// 10.I like websites that have:(Aku suka website yang punya:)
-// a) interesting design and visual features.
-// b) audio channels where I can hear music, radio programs or interviews.
-// c) interesting written descriptions, lists and explanations.
-// d)things I can click on, shift or try.
-// 11.Other than price, what would most influence your decision to buy a new
-// non-fiction book? (Selain harga, apa yang paling mempengaruhi
-// keputusanmu untuk membeli buku nonfiksi?)
-// a) The way it looks is appealing.
-// b)A friend talks about it and recommends it.
-// c) Quickly reading parts of it.
-// d)It has real-life stories, experiences and examples.
-// 12.You are using a book, CD or website to learn how to take photos with your
-// new digital camera.(Kamu sedang menggunakan buku, CD atau website
-// untuk mempelajari bagaimana cara mengambil foto dengan kamera digital
-// barumu).You would liketo have:
-// a) diagrams showing the camera and what each part does.
-// b) a chance to ask questions and talk about the camera and its features.
-// c) clear written instructions with lists and bullet points about what to do.
-// d)many examples of good and poor photos and how to improve them.
-// 13.Do you prefer a teacher or a presenter who uses:(Lebih suka guru atau
-// presenter yang menggunakan:)
-// a) diagrams, charts or graphs.
-// b) question and answer, talk, group discussion, or guest speakers.
-// c) handouts, books, or readings.
-// d) demonstrations, models or practical sessions.
-// 14.You have finished a competition or test and would like some feedback.
-// (Kamu telah menyelesaikan kompetisi atau tes dan menginginkan beberapa
-// timbal balik). You would like to have feedback:
-// a) using graphs showing what you had achieved.
-// b)from somebody who talks it through with you.
-// c) using a written description of your results.
-// d) using examples from what you have done.
-// 15.You are going to choose food at a restaurant or cafe. (Kamu akan memilih
-// makanan di restoran atau kafe). You would:
-// a) look at what others are eating or look at pictures of each dish.
-// b) ask the waiter or friends to recommend choices.
-// c) choose from the descriptions in the menu.
-// d) choose something that you have had there before.
-// 16.You have to make an important speech at a conference or special
-// occasion. (Kamu harus membuat pidato penting di sebuah konferensi atau
-// peristiwa yang spesial). You would:
-// a) make diagrams or get graphs to help explain things.
-// b)write a few key words and practice saying your speech over and over.
-// c) write out your speech and learn from reading it over several times.
-// d) gather many examples and stories to make the talk real and practical.
